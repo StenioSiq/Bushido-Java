bca6395be2da03e55875d8886efd8eb62a1c5915
Stênio Siqueira <steniosqr@gmail.com>
2025-04-29 21:57:43 -0300

    Ex_08 dio Feito!

--- src/practice/exercise/dio_dienens_chaves/ex_08/Main.java
+++ src/practice/exercise/dio_dienens_chaves/ex_08/Main.java
@@ -1,21 +1,29 @@
 package practice.exercise.dio_dienens_chaves.ex_08;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        int [] numbers = new int[3];
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Entre com três números:three numbers: ");
 
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = scanner.nextInt();
+        }
 
-        int x;
+        Arrays.sort(numbers);
 
 
+        for (int i = 0; i < numbers.length /2 ; i++) {
+            int j = numbers[i];
+            numbers[i] = numbers[numbers.length - i - 1];
+            numbers[numbers.length - 1 - i] = j;
 
+            System.out.println(Arrays.toString(numbers));
+        }
     }
 }
